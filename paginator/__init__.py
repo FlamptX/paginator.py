06343a162eb6b3a9ebc8602bf50a75b866712556
FlamptX <dino.shark2006@gmail.com>
2021-10-26 17:37:32 +0200

    Changed restricted_user to author, switched to TypeError, and other minor changes

--- paginator/__init__.py
+++ paginator/__init__.py
@@ -1,11 +1,10 @@
 from .paginator import *
 from .objects import *
-from .errors import *
 
 __title__ = 'paginator.py'
 __summary__ = 'Discord paginator for messages and embeds.'
 __uri__ = 'https://github.com/FlamptX/paginator.py'
 __author__ = 'Flampt'
-__version__ = '0.8'
+__version__ = '0.8.1'
 __license__ = 'MIT'
 __copyright__ = '2021-present Flampt.'
