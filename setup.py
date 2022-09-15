e358ec0c99144dc3d7920d0c4fcdbbc5ae68c450
FlamptX <dino.shark2006@gmail.com>
2022-09-15 22:19:46 +0200

    Fixed interaction issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="paginator.py",
-    version="0.8.3",
+    version="0.9",
     author="Flampt",
     license="MIT",
     description="Simple to use discord paginator for messages and embeds with reactions and buttons.",
